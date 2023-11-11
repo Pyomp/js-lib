52c2ae538041a652432a331de16d9a9d15bded86
n <gayetni@gmail.com>
2023-11-11 15:32:28 +0100

    remove old renderer

--- webGlEngineOld/renderer/modules/types.ts
+++
@@ -1,4 +0,0 @@
-declare type Camera = import("./Camera.js").CameraRenderer;
-declare type Lights = import("./LightsManager.js").LightsManager;
-declare type PointLight = import("./lightsManager/PointLights.js").PointLight;
-declare type State = import("./StateManager.js").StateManager;
