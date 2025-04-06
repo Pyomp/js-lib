c561371b27fdf90ee82656c9af07b05e802b49ec
n <gayetni@gmail.com>
2025-04-06 20:57:51 +0200

    particle emiter

--- 3dEngine/sceneGraph/particle/ParticleEmiter.js
+++ 3dEngine/sceneGraph/particle/ParticleEmiter.js
@@ -1,1 +1,34 @@
-export class ParticleEmiter { }
+import { Line3 } from "../../../math/Line3.js"
+import { Vector3 } from "../../../math/Vector3.js"
+
+export class ParticleEmiter {
+    #direction = new Vector3()
+    #stepDistance
+    #stepDistanceSq
+    newEmiterPosition = new Vector3()
+    previousEmiterPosition = new Vector3()
+    #distanceRest = 0
+    line = new Line3()
+
+    constructor(stepDistance = 0.01) {
+        this.#stepDistance = stepDistance
+        this.#stepDistanceSq = this.#stepDistance ** 2
+    }
+
+    update(/** @type {Vector3} */ newPosition) {
+        this.line.end.copy(newPosition)
+
+        const lineDistance = this.line.distance()
+        this.#direction.subVectors(this.line.end, this.line.start)
+        this.#direction.divideScalar(lineDistance)
+
+        this.#distanceRest += lineDistance
+
+        while (this.#distanceRest >= this.#stepDistance) {
+            // move line.start allong the line
+            this.line.at(this.#stepDistance, this.line.start)
+
+            this.#distanceRest -= this.#stepDistance
+        }
+    }
+}
