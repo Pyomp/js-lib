6b4f82f954952e08835c1fb40b849a63b57758ae
Nicolas Gayet <gayetni@gmail.com>
2025-05-06 21:08:44 +0200

    add immediate

--- utils/EventSet.js
+++ utils/EventSet.js
@@ -1,18 +1,33 @@
 export class EventSet extends Set {
-    emit(...data) {
+    emit(
+        /** @type {any[]} */ ...data
+    ) {
         for (const cb of this) cb(...data)
     }
-    addOnce(callback) {
+
+    addOnce(
+        /** @type {Function} */ callback
+    ) {
         const wrap = () => {
             callback()
             this.delete(wrap)
         }
         this.add(wrap)
     }
-    addUntilTrue(callback) {
+
+    addUntilTrue(
+        /** @type {Function} */ callback
+    ) {
         const wrap = () => {
             if (callback() === true) this.delete(wrap)
         }
         this.add(wrap)
+    }
+
+    addImmediate(
+        /** @type {Function} */ callback
+    ) {
+        callback()
+        this.add(callback)
     }
 }
